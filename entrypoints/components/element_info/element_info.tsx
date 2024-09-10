b7334708628fb489de3ddd0ca9f748d686090e16
saunju <2506152074@qq.com>
2024-09-10 16:42:32 +0800

    删除eslint

--- entrypoints/components/element_info/element_info.tsx
+++ entrypoints/components/element_info/element_info.tsx
@@ -5,6 +5,7 @@ import { useElementCardStore } from "@/entrypoints/store/element_card";
 
 export const ElementInfoId = "element-info";
 const cardWidth = 336;
+const cardHeight = 200; // 假设卡片的高度为 200px
 
 const useCardFollow = (setElementCardPosition: (top: number, left: number) => void) => {
     const { isListeningMouse } = useMouseStore();
@@ -12,14 +13,24 @@ const useCardFollow = (setElementCardPosition: (top: number, left: number) => vo
     const handleMouseMove = useCallback((event: MouseEvent) => {
         requestAnimationFrame(() => {
             const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
             const offset = 40;
+            
             // 判断左右半屏
             const isLeftSide = event.pageX < windowWidth / 2;
+            // 判断上下半屏
+            const isTopSide = event.pageY < windowHeight / 2;
+
             // 根据鼠标位置决定卡片的 left 值
             const left = isLeftSide
                 ? event.pageX + offset
                 : event.pageX - offset - cardWidth;
-            setElementCardPosition(event.pageY + offset, left);
+            // 根据鼠标位置决定卡片的 top 值
+            const top = isTopSide
+                ? event.pageY + offset
+                : event.pageY - offset - cardHeight;
+
+            setElementCardPosition(top, left);
         });
     }, [setElementCardPosition]);
 
